8e9985967284a32e9447b4f6068a452fb79f90ea
anika557 <ummehabiba19758@gmail.com>
2025-08-13 16:00:35 +0600

    day- 19 complete

--- day-18/factorial.c
+++ day-18/factorial.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 int main(){
-    int n,i,fact = 1;
-    printf("Enter any possitive number: ");
+    int n;
+    int i;
+    int fact = 1,facts;
     
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        fact = fact*i;
+        facts = fact*i;
     }
-    printf("%d\n",fact);
+    printf("%d\n",facts);
     return 0;
 }
