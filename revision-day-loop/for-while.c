374984a4c69b4b1bd51dea1f095645292bc63b1f
anika557 <ummehabiba19758@gmail.com>
2025-08-21 10:23:48 +0600

    day-23

---
+++ revision-day-loop/for-while.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    // for (i = 1; i <= 100; i++)
+    // {
+    //     printf("%d\n", i);
+    // }
+    // for(i=100;i>=1;i--)
+    // {
+    //     printf("%d\n",i);
+    // }
+
+    // while loop ar use
+    // i = 1;
+    // while (i <= 100)
+    // {
+    //     printf("%d\n", i);
+    //     i++;
+    // }
+    i=100;
+    while (i>=1)
+    {
+        printf("%d\n",i);
+        i--;
+    }
+    
+
+
+    return 0;
+}
