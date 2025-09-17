e9a6d9988f88a364ae92375241c7462e0064f14f
anika557 <ummehabiba19758@gmail.com>
2025-09-17 20:27:03 +0600

    day-28 complete

---
+++ day-28/pattern-19.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int n,row,col;
+    scanf("%d",&n);
+    for(row=1;row<=n;row++){
+        for(col=1;col<=n;col++){
+            if(col==1 || row==n || row==col)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
